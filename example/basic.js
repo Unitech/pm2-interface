6fdc0f8196842cc3c8efa41656818bbcb21ccf19
tknew2 <strzelewicz.alexandre@gmail.com>
2013-12-20 02:56:17 +0100

    psh

--- example/basic.js
+++ example/basic.js
@@ -1,21 +1,39 @@
-var ipm2 = require('..')();
+var ipm2 = require('..');
 
-ipm2.on('ready', function() {
+var ipm2a = ipm2({bind_host : 'localhost'});
+
+
+
+ipm2a.on('ready', function() {
   console.log('Connected to pm2');
 
-  ipm2.bus.on('*', function(event, data){
+  ipm2a.bus.on('*', function(event, data){
     console.log(event);
   });
 
-
-  setTimeout(function() {
-    ipm2.rpc.restartProcessId(0, function(err, dt) {
-      console.log(dt);
-    });
-  }, 2000);
-
-
-  ipm2.rpc.getMonitorData({}, function(err, dt) {
-    console.log(dt);
+  ipm2a.on('rpc_sock:reconnecting', function() {
+    console.log('rpc_sock:reconnecting');
   });
+  ipm2a.on('sub_sock:ready', function() {
+    console.log('sub_sock:ready');
+  });
+  ipm2a.on('sub_sock:closed', function() {
+    console.log('sub_sock:closed');
+  });
+
+  ipm2a.on('sub_sock:reconnecting', function() {
+    console.log('sub_sock:reconnecting');
+  });
+
+
+  // setTimeout(function() {
+  //   ipm2.rpc.restartProcessId(0, function(err, dt) {
+  //     console.log(dt);
+  //   });
+  // }, 2000);
+
+
+  // ipm2.rpc.getMonitorData({}, function(err, dt) {
+  //   console.log(dt);
+  // });
 });

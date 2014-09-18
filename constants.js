a57e21e6b89cec0e8ef70d899f48422367a7817a
tknew2 <strzelewicz.alexandre@gmail.com>
2014-09-18 19:17:38 +0200

    2.0.1 - UNIX SOCKET SUPPORT

--- constants.js
+++ constants.js
@@ -3,10 +3,14 @@
 // pm2-interface intercommunication (because of ports)
 //
 
+var p    = require('path');
+var HOME = process.env.PM2_HOME || process.env.HOME;
+var DEFAULT_FILE_PATH = p.resolve(HOME, '.pm2');
+
 module.exports = {
   DAEMON_BIND_HOST   : 'localhost',
-  DAEMON_RPC_PORT    : 6666, // RPC commands
-  DAEMON_PUB_PORT    : 6667, // Realtime events
+  DAEMON_RPC_PORT    : p.join(DEFAULT_FILE_PATH, 'rpc.sock'), // RPC commands
+  DAEMON_PUB_PORT    : p.join(DEFAULT_FILE_PATH, 'pub.sock'), // Realtime events
   SUCCESS_EXIT       : 0,
   ERROR_EXIT         : 1
 };
